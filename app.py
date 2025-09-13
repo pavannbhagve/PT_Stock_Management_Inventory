f0ef6658579ffa07cc13a0512c6d799d057925cd
Pavan Narayan Bhagve <163168267+pavannbhagve@users.noreply.github.com>
2025-09-14 02:12:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -256,4 +256,6 @@ def process_request(req_id):
 # Run App
 # -------------------
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use Render PORT or default 5000
+    app.run(host="0.0.0.0", port=port)
     app.run(debug=True)
